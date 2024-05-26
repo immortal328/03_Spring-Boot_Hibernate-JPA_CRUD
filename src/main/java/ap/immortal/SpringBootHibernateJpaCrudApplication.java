eeb1cdd531be24065a0b47a2ff3006a45fa14e1a
Amar H. Pujari. <47078596+immortal328@users.noreply.github.com>
2024-05-27 00:01:10 +0530

    Initial commit

---
+++ src/main/java/ap/immortal/SpringBootHibernateJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package ap.immortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateJpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateJpaCrudApplication.class, args);
+	}
+
+}
